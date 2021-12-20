49189f1b75003112be1cf5d38007a84f4a59f1e4
KimTaeHyen <62549012+teahyen@users.noreply.github.com>
2021-12-20 09:15:35 +0900

    처음부터 하나하나 다시 돌아가서 하는 중
    일단 지금 버튼이 안눌려요

--- Among_Us/Assets/01.Script/Utill/TasksScript/CombinationScript/ConnectLine.cs
+++ Among_Us/Assets/01.Script/Utill/TasksScript/CombinationScript/ConnectLine.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ConnectLine : MonoBehaviour
 {
     public Image lineImg;
-    public Transform startPos;
+    public Button startPos;
     private Vector3 mousePos;
 
     //여기서 끝 마무리 결정
@@ -28,54 +28,56 @@ public class ConnectLine : MonoBehaviour
         print(p.name);
         if (p.name.Contains("End"))
             Destroy(this);
+        startPos.onClick.AddListener(() =>
+        {
+            print("실행됨");
+            lineImg.gameObject.SetActive(true);
+            mousePos = Input.mousePosition;
+            lineImg.transform.position = startPos.transform.position;
+            headLine.transform.position = startPos.transform.position;
+        });
     }
 
     void Update()
     {
-        //조건을 추가해서 일부 범위에서만 눌릴 수 있도록 조건을 만들어야해ㅐ
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit rayHit;
-            if (Physics.Raycast(ray, out rayHit))
-            {
-                if (rayHit.transform.gameObject.CompareTag("LineBtn"))
-                {
-                    
-                    lineImg.gameObject.SetActive(true);
-                    mousePos = Input.mousePosition;
-                    lineImg.transform.position = startPos.position;
-                    headLine.transform.position = startPos.position;
-                }
-            }
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    lineImg.gameObject.SetActive(true);
+        //    mousePos = Input.mousePosition;
+        //    lineImg.transform.position = startPos.transform.position;
+        //    headLine.transform.position = startPos.transform.position;
+        //}
+
         //이때 머리 부분도 같이 움직이게 해야한다.
         if (Input.GetMouseButton(0))
         {
             Vector3 myPos = Input.mousePosition;
-             lineImg.transform.localScale = new Vector2(Vector3.Distance(myPos, mousePos) /55                                            , 1);
+            lineImg.transform.localScale = new Vector2(Vector3.Distance(myPos, mousePos)/30, 1);
             //여기 아래 부분이랑 똑같이 하면 될거 같은데
             lineImg.transform.localRotation = Quaternion.Euler(0, 0,
                 AngleInDeg(mousePos, myPos));
+
+
+            headLine.transform.localPosition = myPos;
             headLine.transform.localRotation = Quaternion.Euler(0, 0,
                 AngleInDeg(mousePos, myPos));
 
         }
 
-        isEnd = Physics2D.OverlapCircle(transform.position, connectRange, endLayer);
-        endCol = Physics2D.OverlapCircle(transform.position, connectRange, endLayer);
+        //isEnd = Physics2D.OverlapCircle(transform.position, connectRange, endLayer);
+        //endCol = Physics2D.OverlapCircle(transform.position, connectRange, endLayer);
         
-        if (Input.GetMouseButtonUp(0)&&!isEnd)
+        if (Input.GetMouseButtonUp(0)/*&&!isEnd*/)
         {
             //만약 연결이 됬으면 이 함수를 실행 시키면 안된다.
             lineImg.gameObject.SetActive(false);
-            headLine.transform.position = startPos.position;
+            headLine.transform.position = startPos.transform.position;
         }
-        else if(Input.GetMouseButtonUp(0) && isEnd)
-        {
-            lineImg.gameObject.SetActive(true);
-            headLine.transform.position = endCol.transform.position;
-        }
+        //else if(Input.GetMouseButtonUp(0) && isEnd)
+        //{
+        //    lineImg.gameObject.SetActive(true);
+        //    headLine.transform.position = endCol.transform.position;
+        //}
     }
 
     public static float AngleInRad(Vector3 vec1, Vector3 vec2)
@@ -88,12 +90,12 @@ public class ConnectLine : MonoBehaviour
         return AngleInRad(vec1, vec2) * 180 / Mathf.PI;
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        for (int i = 1; i <= 4; i++)
-        {
-            Gizmos.DrawWireSphere(endObj[i].transform.position, connectRange);
-        }
-    }
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Gizmos.color = Color.yellow;
+    //    for (int i = 1; i <= 4; i++)
+    //    {
+    //        Gizmos.DrawWireSphere(endObj[i].transform.position, connectRange);
+    //    }
+    //}
 }
